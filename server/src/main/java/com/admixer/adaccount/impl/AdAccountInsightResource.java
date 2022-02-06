8cd70c227a1a2a5fe3d8b2f70ca48a3b8f46f38d
jfeng1115 <jfeng1115@gmail.com>
2022-02-06 13:31:14 -0800

    Update AdAccountInsightResource get(long key) method

--- server/src/main/java/com/admixer/adaccount/impl/AdAccountInsightResource.java
+++ server/src/main/java/com/admixer/adaccount/impl/AdAccountInsightResource.java
@@ -8,5 +8,21 @@ import com.admixer.adaccount.AdAccountInsight;
 
 @RestLiCollection(name = "adAccountInsights", namespace = "com.admixer.adaccount")
 public class AdAccountInsightResource extends CollectionResourceTemplate<Long, AdAccountInsight>  {
-    
+    static Map<Long, AdAccountInsight> adAccountInsights = new HashMap<>();
+    static {
+        adAccountInsights.put(1L, new AdAccountInsight().setAdAccountId(12L).setImpressionCount(10002L));
+        adAccountInsights.put(2L, new AdAccountInsight().setAdAccountId(1234L).setImpressionCount(999L));
+    }
+
+    public AdAccountInsight get(Long key){
+        AdAccountInsight adAccountInsight = adAccountInsights.get(key);
+        if (adAccountInsight == null) {
+            return null;
+        }
+        return new AdAccountInsight()
+                .setAdAccountId(adAccountInsight.getAdAccountId())
+                .setImpressionCount(adAccountInsight.getImpressionCount());
+
+    }
+
 }
